6c1a665bcf829ef55d9e47dc34937c98a67b41f4
Nicolas <nicolasnahuel01@gmail.com>
2022-06-21 17:05:00 -0300

    Starting 4th cert

---
+++ 4th Certification/Debugging.js
@@ -0,0 +1,10 @@
+// se recomienda usar chrome o mozilla como browsers
+console.log();
+console.clear();
+
+//You can use typeof to check the data structure, or type, of a variable
+console.log(typeof ""); //string
+console.log(typeof 0); //number
+console.log(typeof []); //object. Note that in JavaScript, arrays are technically a type of object
+console.log(typeof {}); //object. 
+//JavaScript recognizes seven primitive (immutable) data types: Boolean, Null, Undefined, Number, String, Symbol, and BigInt, and one type for mutable items: Object.
