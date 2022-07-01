cd22b924179d667b44774fe03f6d8acf2b0c3b12
Nicolas <nicolasnahuel01@gmail.com>
2022-07-01 10:21:17 -0300

    pushing prototype

--- 7th Certification/prototype.js
+++ 7th Certification/prototype.js
@@ -1,3 +1,51 @@
+//  prototype.   Properties in the prototype are shared among ALL instances of Bird.
+// All instances automatically have the properties on the prototype.
+function Bird(name) {
+    this.name = name; //own property
+}
+
+Bird.prototype.numLegs = 2; // prototype property
+
+let ducks = new Bird("Donald");
+
+//A more efficient way is to set the prototype to a new object that already contains the properties. 
+Bird.prototype = {
+    constructor: Bird, //always define the constructor property:
+    numLegs: 2,
+    eat: function() {
+        console.log("nom nom nom");
+    },
+    describe: function() {
+        console.log("My name is " + this.name);
+    }
+};
+
+// isPrototypeOf()
+Bird.prototype.isPrototypeOf(duck);
+
+//Here is how you add duck's own properties to the array ownProps and prototype properties to the array prototypeProps:
+
+ownProps = [];
+let prototypeProps = [];
+
+for (let property in ducks) {
+    if (ducks.hasOwnProperty(property)) {
+        ownProps.push(property);
+    } else {
+        prototypeProps.push(property);
+    }
+}
+
+console.log(ownProps); //["name"]
+console.log(prototypeProps); //["numLegs"]
+
+//constructor property is a reference to the constructor function that created the instance. 
+//The advantage of the constructor property is that it's possible to check for this property to find out what kind of object it is.
+duck = new Bird();
+
+console.log(duck.constructor === Bird);
+
+
 /*
 Because a prototype is an object, a prototype can have its own prototype! 
 In this case, the prototype of Bird.prototype is Object.prototype:
@@ -6,7 +54,7 @@ In this case, the prototype of Bird.prototype is Object.prototype:
 function Bird(name) {
     this.name = name;
 }
-  
+
 typeof Bird.prototype;
 
 Object.prototype.isPrototypeOf(Bird.prototype);
@@ -23,25 +71,25 @@ Therefore, any object can use the hasOwnProperty method. */
 
 function Cat(name) {
     this.name = name;
-  }
-  
-  Cat.prototype = {
+}
+
+Cat.prototype = {
     constructor: Cat,
-  };
-  
-  function Bear(name) {
+};
+
+function Bear(name) {
     this.name = name;
-  }
-  
-  Bear.prototype = {
+}
+
+Bear.prototype = {
     constructor: Bear,
-  };
-  
-  function Animal() { }
-  
-  Animal.prototype = {
+};
+
+function Animal() {}
+
+Animal.prototype = {
     constructor: Animal,
     eat: function() {
-      console.log("nom nom nom");
+        console.log("nom nom nom");
     }
-  };
+};
