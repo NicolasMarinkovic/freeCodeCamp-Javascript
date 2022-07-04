9fe47ce00282528e9b1f42ee8383d36c9b8ef578
Nicolas <nicolasnahuel01@gmail.com>
2022-07-04 17:37:31 -0300

    Going trhow 8th cert

--- 8th Certification- Functional Programming/test.js
+++ 8th Certification- Functional Programming/test.js
@@ -1,43 +1,24 @@
-// tabs is an array of titles of each site open within the window
-const Window = function(tabs) {
-    this.tabs = tabs; // We keep a record of the array inside the object
-};
-  
-// When you join two windows into one window
-Window.prototype.join = function(otherWindow) {
-    this.tabs = this.tabs.concat(otherWindow.tabs);
-    return this;
-};
-  
-// When you open a new tab at the end
-Window.prototype.tabOpen = function(tab) {
-    this.tabs.push('new tab'); // Let's open a new tab for now
-    return this;
-};
-  
-// When you close a tab
-Window.prototype.tabClose = function(index) {
-  
-    // Only change code below this line
-    const tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
-    const tabsAfterIndex = this.tabs.splice(1,this.tabs.length); // Get the tabs after the tab
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
 
-    this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
-  
-    // Only change code above this line
-  
-    return this;
-};
-  
-// Let's create three browser windows
-const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
-const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
-const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
-  
-// Now perform the tab opening, closing, and other operations
-const finalTabs = socialWindow
-    .tabOpen() // Open a new tab for cat memes
-    .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
-    .join(workWindow.tabClose(1).tabOpen());
+// Change code below this line
+function add(arr, bookName) {
+    let newArr = [...arr];
+    newArr.push(bookName);
+    return newArr;
 
-console.log(finalTabs.tabs);
+    // Change code above this line
+}
+
+// Change code below this line
+function remove(arr, bookName) {
+    let newArr = [...arr];
+    if (newArr.indexOf(bookName) >= 0) {
+        newArr.splice(newArr.indexOf(bookName), 1);
+        return newArr;
+        // Change code above this line
+    }
+}
+let newArr = add(bookList, "A Brief History of Time");
+let newArrs = remove(bookList, "Disquisitiones Arithmeticae");
+console.log(newArr, newArrs);
